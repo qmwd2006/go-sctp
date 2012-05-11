32102e1105abefefbf51e3385bc8511aff964c4f
Olivier Van Acker <cyberroadie@gmail.com>
2012-05-11 18:38:34 +0100

    set SCTP_LISTEN_FIX for darwin so pollServer will get its event

--- src/pkg/net/sctpsock.go
+++ src/pkg/net/sctpsock.go
@@ -279,31 +279,6 @@ func ListenOneToOneSCTP(net string, laddr *SCTPAddr) (*SCTPListener, error) {
   return l, nil
 }
 
-// ListenSCTP can be used for one-to-many style SCTP sockets 
-func ListenSCTP(net string, laddr *SCTPAddr) (conn *SCTPConn, err error) {
-  switch net {
-  case "sctp", "sctp4", "sctp6":
-  default:
-    return nil, UnknownNetworkError(net)
-  }
-  if laddr == nil {
-    return nil, &OpError{"listen", net, nil, errMissingAddress}
-  }
-	fd, err := internetSocket(net, laddr.toAddr(), nil, syscall.SOCK_SEQPACKET, syscall.IPPROTO_SCTP, "listen", sockaddrToSCTP)
-	if err != nil {
-		return nil, err
-	}
-  conn = newSCTPConn(fd)
-  conn.SetInitMsg()
-
-	err = syscall.Listen(fd.sysfd, listenerBacklog)
-	if err != nil {
-		closesocket(fd.sysfd)
-		return nil, &OpError{"listen", net, laddr, err}
-	}
-	return conn, nil
-}
-
 // AcceptSCTP accepts the next incoming call and returns the new connection
 // and the remote address.
 func (l *SCTPListener) AcceptSCTP() (c *SCTPConn, err error) {
